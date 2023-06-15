39d176b90a1083731e26353d6049748a65a6853d
Nizar <n.bahajoub@gmail.com>
2023-06-14 17:59:28 -0700

    pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,15 @@ def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return (0)
     else:
-        rom = dict(I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000)
+        rom = {
+                I = 1,
+                V = 5,
+                X = 10,
+                L = 50,
+                C = 100,
+                D = 500,
+                M = 1000
+        }
         total = 0
         for i in range(len(roman_string) - 1):
             if rom[roman_string[i]] < rom[roman_string[i + 1]]:
