6cd339eff219d1379fc321a40742cb74611b7490
Nizar <n.bahajoub@gmail.com>
2023-06-26 18:35:43 -0700

    Task 3 adding an 'l'

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -5,6 +5,6 @@ def safe_print_division(a, b):
     except (TypeError, ZeroDivisionError):
         result = None
     finally:
-        print("Inside resut: {}".format(result))
+        print("Inside result: {}".format(result))
 
     return (result)
