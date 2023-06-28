f515ec22f33f91b8a232b631f082e4ce77ed4de5
Nizar <n.bahajoub@gmail.com>
2023-06-27 18:42:41 -0700

    Task 2 output modified

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -19,4 +19,4 @@ class Square:
             else:
                 raise ValueError("size must be >= 0")
         else:
-            raise TypeError("size must be integer")
+            raise TypeError("size must be an integer")
