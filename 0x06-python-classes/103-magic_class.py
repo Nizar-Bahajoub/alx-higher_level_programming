65dae63a86c0efb0af747f17459f83996d621a92
Nizar <n.bahajoub@gmail.com>
2023-06-28 17:49:39 -0700

    Advanced 4

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import math
+
+
+class MagicClass:
+    def __init__(self, radius):
+        self.__radius = None
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        return (self.__radius * 2 * math.pi)
