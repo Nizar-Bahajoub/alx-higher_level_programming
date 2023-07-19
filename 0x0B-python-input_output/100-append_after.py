4d54eb81665686c50287fba4b694eaa17cc490f4
Nizar <n.bahajoub@gmail.com>
2023-07-19 09:13:24 -0700

    Advanced 1

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""insert after a string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    new_txt = ""
+    with open(filename) as f:
+        for i in f:
+            new_txt += i
+            if search_string in i:
+                new_txt += new_string
+    with open(filename, "w") as a:
+        a.write(new_txt)
