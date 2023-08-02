30c9b2b193d9ff3c3239d5d4c8f54a17e5f9c785
Nizar <n.bahajoub@gmail.com>
2023-08-02 04:28:09 -0700

    Task 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,13 +26,6 @@ class Base:
             return ("[]")
         return (json.dumps(list_dictionaries))
 
-    @staticmethod
-    def from_json_string(json_string):
-        """returns the list of the JSON string representation json_string"""
-        if json_string is None or len(json_string) is 0:
-            return ({})
-        return (json.loads(json_string))
-
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
@@ -49,3 +42,17 @@ class Base:
 
         with open(filename, "w") as f:
             f.write(lists)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation json_string"""
+        if json_string is None or len(json_string) == 0:
+            return ([])
+        return (json.loads(json_string))
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        dum = cls(10, 10)
+        dum.update(**dictionary)
+        return (dum)
