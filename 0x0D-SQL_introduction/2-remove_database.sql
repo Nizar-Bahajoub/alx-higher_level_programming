301a0067a9696e7351ac15d05c6a78d468bb4b00
Nizar <n.bahajoub@gmail.com>
2023-08-15 15:46:36 -0700

    Task 4 to 7

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Delate a Database
 -- should not fail if it doesnt' exist
-DROP DATABASE hbtn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- adds a row to first_table
+INSERT INTO first_table
+VALUES (89, 'Best School');
