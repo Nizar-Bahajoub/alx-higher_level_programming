6cd6fbf277376bedaa98b54369425d1edc432363
Nizar <n.bahajoub@gmail.com>
2023-09-16 11:48:25 -0700

    shebang added

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""
+This script defines a State class and
+a Base class to work with MySQLAlchemy ORM
+"""
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -10,6 +17,6 @@ class State(Base):
 
     __tablename__ = 'states'
 
-    id = Column(Integer, nullable=False, primary_key=True,
+    id = Column(Integer, primary_key=True,
                 autoincrement=True)
     name = Column(String(128), nullable=False)
