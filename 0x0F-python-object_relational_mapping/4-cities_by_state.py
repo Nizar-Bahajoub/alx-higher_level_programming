5dda21141b4fdf6eafac107a5b73c811359be39d
Nizar <n.bahajoub@gmail.com>
2023-09-16 08:27:15 -0700

    Error hudling

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,9 +15,8 @@ if __name__ == "__main__":
 
     cur = db.cursor()
 
-    query = "SELECT cities.id, cities.name AS city_name, \
-            tates.name AS state_name \
-            FROM cities JOIN states ON cities.state_id = states.id\
+    query = "SELECT cities.id, cities.name, states.name \
+            FROM cities JOIN states ON cities.state_id = states.id \
             ORDER BY cities.id ASC"
     cur.execute(query)
 
