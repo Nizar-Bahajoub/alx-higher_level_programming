ab97fb55e72d450e9218c0196dfa6f62500aab8b
Nizar <n.bahajoub@gmail.com>
2023-09-16 06:49:20 -0700

    Error handling

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,20 +10,26 @@ from sys import argv
 
 if __name__ == "__main__":
     """SQL query using MySQLdb"""
-    db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    try:
+        db = MySQLdb.connect(
+            host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 
-    cur = db.cursor()
+        cur = db.cursor()
 
-    query = "SELECT * FROM states WHERE name = '{}'\
-            ORDER BY id".format(argv[4])
-    cur.execute(query)
+        query = "SELECT * FROM states WHERE name = '{}'\
+                ORDER BY id".format(argv[4])
+        cur.execute(query)
 
-    rows = cur.fetchall()
+        rows = cur.fetchall()
 
-    for row in rows:
-        print(row)
+        for row in rows:
+            print(row)
 
-    cur.close()
+    except MySQLdb.Error as e:
+        print("Error: {}".format(e))
 
-    db.close()
+    finally:
+        if cur:
+            cur.close()
+        if db:
+            db.close()
