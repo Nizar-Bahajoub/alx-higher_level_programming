f5eb07a678e6fc6765e1bb3cb2976c79456958a9
Nizar <n.bahajoub@gmail.com>
2023-09-16 05:53:43 -0700

    Pycodestyle sheched

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,6 +6,7 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """the main code"""
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3])
 
