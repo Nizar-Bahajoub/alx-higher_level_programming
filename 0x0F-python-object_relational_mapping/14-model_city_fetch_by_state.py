5a9d1159e6d83b9795d1270ab9fbe699dcc9324d
Nizar <n.bahajoub@gmail.com>
2023-09-17 09:05:21 -0700

    Task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     output = session.query(City, State).join(State)
 
     for city, state in output.all():
-        print("{}: ({}) ({})".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.commit()
     session.close()
