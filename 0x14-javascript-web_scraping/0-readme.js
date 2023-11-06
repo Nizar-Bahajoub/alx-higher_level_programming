f620d77e0b70cea93171093dac7b48d137c17ad3
Nizar Bahajoub <n.bahajoub@gmail.com>
2023-11-06 05:45:27 -0800

    Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,16 +2,16 @@
 const fs = require('fs');
 
 if (process.argv.length < 2) {
-	console.log('Please proide the file path as an argument.');
-	process.exit(1);
+  console.log('Please proide the file path as an argument.');
+  process.exit(1);
 }
 
 const filepath = process.argv[2];
 
 fs.readFile(filepath, 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
 });
